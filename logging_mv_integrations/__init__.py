4351993c87e988a33d10b9534533b284f8a4b6b5
Jeff Tanner <jefft@tune.com>
2017-11-21 16:47:45 -0800

    [MVC-114] (logging-mv-integrations) pytests

--- logging_mv_integrations/__init__.py
+++ logging_mv_integrations/__init__.py
@@ -4,8 +4,8 @@
 #  @namespace logging_mv_integrations
 
 __title__ = 'logging-mv-integrations'
-__version__ = '0.3.05'
-__build__ = 0x000305
+__version__ = '0.3.06'
+__build__ = 0x000306
 __version_info__ = tuple(__version__.split('.'))
 
 __author__ = 'jefft@tune.com'
